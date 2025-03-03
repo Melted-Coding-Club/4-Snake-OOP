e6ef33e766fb2133ad597f7f8a9058a9cf3dd737
MeltedButter77 <meltedbuttergaming@gmail.com>
2025-03-03 11:33:36 +1030

    Created Snake OOP

--- main.py
+++ main.py
@@ -1,140 +1,200 @@
 import pygame
 import random
+import copy
 
 pygame.init()
 
-screen = pygame.display.set_mode((600, 600))
-grid_size = 30
-
-# Create a "timer_event" which will be triggered evey "time_delay" milliseconds. This will act as an event which will be processed in the event loop.
-time_delay = 200
-timer_event = pygame.USEREVENT + 1
-pygame.time.set_timer(timer_event, time_delay)
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+GRID_SIZE = 40
+FPS = 60
+MOVE_DELAY = 250  # Milliseconds
 
 
-# Create the main "Snake" class, this will house the repeated logic for each snake.
+class Apple:
+    def __init__(self, screen, grid_size=40):
+        self.screen = screen
+        self.grid_size = grid_size
+        self.rect = None
+        self.spawn_new()
+
+    def spawn_new(self, snakes=None):
+        valid_position = False
+        while not valid_position:
+            new_x = random.randint(0, (self.screen.get_width() - self.grid_size) // self.grid_size) * self.grid_size
+            new_y = random.randint(0, (self.screen.get_height() - self.grid_size) // self.grid_size) * self.grid_size
+            self.rect = pygame.Rect(new_x, new_y, self.grid_size, self.grid_size)
+
+            if not snakes or all(not segment.colliderect(self.rect) for snake in snakes for segment in snake.body):
+                valid_position = True  # Found a valid position, exit loop
+
+    def render(self):
+        pygame.draw.rect(self.screen, "red", self.rect)
+
+
 class Snake:
-    # __init__ is the constructor for any class. It is always required. This one has "location" and "controls" set as inputs.
-    def __init__(self, location, controls):
-        # self.variable will create an attribute (which is a variable) generic to this class, meaning any objects made with this class will have these attributes.
+    def __init__(self, location, controls, move_event, screen, grid_size=40):
+        self.grid_size = grid_size
+        self.move_event = move_event
+        self.screen = screen
+        self.starting_location = copy.copy(location)
 
-        # Store the head as a separate Rect object and the body as a list of Rect objects.
-        self.head = pygame.Rect(location, (grid_size, grid_size))
+        self.is_dead = None
         self.body = [pygame.Rect((location[0] + grid_size, location[1]), (grid_size, grid_size))]
-
-        # Controls is set as its input
         self.controls = controls
 
-        # These booleans are hardcoded as the default state of the snake.
-        self.isAlive = True
-        self.direction = None
+        self.current_direction = "right"
+        self.next_direction = "right"
 
-    # Instead of handling each event separately in the event loop, we will pass them to this "handle_event" method.
-    # This way we can separate our code (so that everything to do with the Snake class is in one place) making it easier to understand.
-    def handle_event(self, event):
-        # If the snake is dead, we will not process any events and instead return nothing.
-        if not self.isAlive:
+    def handle_event(self, event, snakes, apples):
+        if self.is_dead:
             return
 
-        # Process a KEYDOWN event
         if event.type == pygame.KEYDOWN:
-            if event.key == self.controls[0]:
-                self.direction = 'left'
-            elif event.key == self.controls[1]:
-                self.direction = 'right'
-            elif event.key == self.controls[2]:
-                self.direction = 'up'
-            elif event.key == self.controls[3]:
-                self.direction = 'down'
+            if event.key == self.controls["right"]:
+                if not self.current_direction == 'left':
+                    self.next_direction = "right"
+            if event.key == self.controls["left"]:
+                if not self.current_direction == 'right':
+                    self.next_direction = "left"
+            if event.key == self.controls["up"]:
+                if not self.current_direction == 'down':
+                    self.next_direction = "up"
+            if event.key == self.controls["down"]:
+                if not self.current_direction == 'up':
+                    self.next_direction = "down"
 
-        # Process a timer_event, this will move our snake forward at a constant speed.
-        # Notice, if the snake does not have a direction. This entire move code will not run including collision calculations.
-        elif event.type == timer_event and self.direction:
+        if event.type == self.move_event:
+            direction_offsets = {
+                "right": (self.grid_size, 0),
+                "left": (-self.grid_size, 0),
+                "up": (0, -self.grid_size),
+                "down": (0, self.grid_size),
+            }
 
-            # Moving the snake forward is a 3-step process. This ensures when the snake dies it dies in the correct position.
-            # 1. Add head copy to body
-            # 2. Move the head
-            # 3. Check for collisions on head
+            self.body.insert(0, self.body[0].copy())
+            offset_x, offset_y = direction_offsets[self.next_direction]
+            self.body[0].move_ip(offset_x, offset_y)
+            self.current_direction = self.next_direction
 
+            # Check apple collision
+            for apple in apples:
+                if self.body[0].colliderect(apple.rect):
+                    apple.spawn_new(snakes)
+                    self.body.insert(-1, pygame.Rect(self.body[-1].x, self.body[-1].y, self.grid_size, self.grid_size))
 
-            # Add a copy of head to the body
-            self.body.insert(0, self.head.copy())
+            # Check snake collision with walls
+            if self.body[0].right > (self.screen.get_width()) or self.body[0].left < 0 or self.body[0].top < 0 or self.body[0].bottom > (self.screen.get_height()):
+                # Switch to the dead state
+                self.body.pop(0)
+                self.is_dead = True
+                return
 
-            # Move the head forward
-            if self.direction == 'left':
-                self.head.x -= grid_size
-            elif self.direction == 'right':
-                self.head.x += grid_size
-            elif self.direction == 'up':
-                self.head.y -= grid_size
-            elif self.direction == 'down':
-                self.head.y += grid_size
+            # Check snake collision with other snakes
+            for snake in snakes:
+                if any(self.body[0].colliderect(segment) for segment in snake.body if snake != self):
+                    self.is_dead = True
+                    return
 
-            # Check if new head has made collisions with walls, if not delete end tail
-            if (self.head.centerx > (screen.get_width()) or
-                    self.head.centerx < 0 or
-                    self.head.centery < 0 or
-                    self.head.centery > (screen.get_height())):
-                self.isAlive = False
-            else:
-                # Deleting the tail if there is no collision assures the tail doesn't disappear on death despite movement being invalid.
+            if len(self.body) > 1:
                 self.body.pop()
 
-            # Loop through the apple Rects in the apples list
-            for apple in apples:
-                # check if the head collides with the apple
-                if self.head.colliderect(apple):
-                    apple.x = random.randint(0, (screen.get_width() - grid_size) // grid_size) * grid_size
-                    apple.y = random.randint(0, (screen.get_height() - grid_size) // grid_size) * grid_size
-                    self.body.insert(-1, pygame.Rect(self.body[-1].x, self.body[-1].y, grid_size, grid_size))
+            # Check snake collision with itself
+            for i in range(1, len(self.body)):
+                if self.body[0].colliderect(self.body[i]):
+                    self.is_dead = True
+                    return
 
+    def reset(self):
+        self.body = [pygame.Rect((self.starting_location[0] + self.grid_size, self.starting_location[1]), (self.grid_size, self.grid_size))]
+        self.current_direction = "right"
+        self.next_direction = "right"
+        self.is_dead = False
+
+    def render(self):
+        if not self.is_dead:
             for segment in self.body:
-                # Check for Collisions with the body
-                if self.head.colliderect(segment):
-                    self.isAlive = False
-
-    # A separated draw method is used for a similar reason to the handle_event method.
-    # This allows us to keep the snake logic contained to the class. This is more important later when objects become more complex.
-    def draw(self):
-        if self.isAlive:
-            colour = "green"
+                pygame.draw.rect(self.screen, "blue", segment)
+            pygame.draw.rect(self.screen, "green", self.body[0])
         else:
-            colour = "red"
-
-        pygame.draw.rect(screen, colour, self.head)
-        for rect in self.body:
-            pygame.draw.rect(screen, colour, rect)
+            for segment in self.body:
+                pygame.draw.rect(self.screen, "orange", segment)
 
 
-# The game can be changed my modifying the objects below without having to change the logic above!
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('Snake')
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.SysFont("Segoe UI", 35)
+        self.fps = FPS
+        grid_size = GRID_SIZE
 
-# We have created the class, now we need to create objects. This creates instances (in this case 2) of the Snake class allowing us to make as many as we want without having to repeat the snake's logic.
-snake1 = Snake((5 * grid_size, 5 * grid_size), (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN))
-snake2 = Snake((15 * grid_size, 15 * grid_size), (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s))
-# We add the snakes to a list for easy looping.
-snakes = [snake1, snake2]
+        move_event = pygame.USEREVENT + 1
+        pygame.time.set_timer(move_event, MOVE_DELAY)
 
-# Similar deal with the apples, but they do not need a custom class. A simple Rect object can be used.
-apple1 = pygame.Rect(grid_size * 3, grid_size * 3, grid_size, grid_size)
-apple2 = pygame.Rect(grid_size * 3, grid_size * 3, grid_size, grid_size)
-apples = [apple1, apple2]
+        self.is_over = False
+        self.snakes = [
+            Snake(
+                (2 * grid_size, 2 * grid_size),
+                {
+                    "left": pygame.K_LEFT,
+                    "right": pygame.K_RIGHT,
+                    "up": pygame.K_UP,
+                    "down": pygame.K_DOWN
+                }, move_event, self.screen, grid_size
+            ),
+            Snake(
+                (5 * grid_size, 5 * grid_size),
+                {
+                    "left": pygame.K_a,
+                    "right": pygame.K_d,
+                    "up": pygame.K_w,
+                    "down": pygame.K_s
+                }, move_event, self.screen, grid_size
+            ),
+        ]
+        self.apples = []
+        for i in range(2):
+            self.apples.append(Apple(self.screen, grid_size))
 
-# This is the entire game loop. Look how much smaller and easier it is to read now that we are using objects!
-while True:
-    # Event Loop
-    for event in pygame.event.get():
+    def render(self):
+        self.screen.fill("dark gray")
+        for apple in self.apples:
+            apple.render()
+        for snake in self.snakes:
+            snake.render()
+        if self.is_over:
+            text = self.font.render("Game Over", True, "white")
+            text_rect = text.get_rect(center=(self.screen.get_width() // 2, self.screen.get_height() // 2))
+            self.screen.blit(text, text_rect)
+
+        pygame.display.update()
+        self.clock.tick(self.fps)
+
+    def handle_event(self, event):
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-        for snake in snakes:
-            snake.handle_event(event)
 
-    # Rendering
-    for snake in snakes:
-        snake.draw()
-    for apple in apples:
-        pygame.draw.rect(screen, "red", apple)
+        for snake in self.snakes:
+            snake.handle_event(event, self.snakes, self.apples)
 
-    # Don't forget to update the screen after rendering
-    pygame.display.update()
-    screen.fill("gray")
+        if all(snake.is_dead for snake in self.snakes):
+            self.is_over = True
+
+        if self.is_over and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            for snake in self.snakes:
+                snake.reset()
+            self.is_over = False
+            for apple in self.apples:
+                apple.spawn_new(self.snakes)
+
+    def run(self):
+        while True:
+            # Event handling
+            for event in pygame.event.get():
+                self.handle_event(event)
+            self.render()
+
+
+Game().run()
